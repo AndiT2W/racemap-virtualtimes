70f68d5b70eef9c87ba9b051c301f0cfe702b99a
Andi <andi@time2win.at>
2021-12-29 16:35:43 +0100

    fixed bug with doubled entries with live data feed, changed everything to local time (see config.js)

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ async function saveRaceMapTimes(respData){
             // calc interpolated time from gps points
             filterTimes(timeArray, function(filteredTimeArray){
                 
-                _.forEach(filteredTimeArray, function(value, key){
+                _.forEach(filteredTimeArray, async function(value, key){
 
                     if (value != null)
                     {                    
@@ -136,22 +136,79 @@ async function saveRaceMapTimes(respData){
     
                         // look if we have for the hash timekeeping point a box id
                         var findElem = _.find(db.data.timekeepings, {'id': timeKeepingId});
-    
+                            
                         if (findElem == undefined)
                         {
                             console.log('ERROR: found no boxId for timekeeping Id:' + timeKeepingId);
                         } else {                    
                             timeObj.boxId = findElem.boxId;
+                            //timeObj.updateTime = new Date().toISOString();
+                            timeObj.updateTime = await getCurrentISOString(config.addSeconds2Time * 1000);
                             
                             // search item
-                            var index = _.findIndex(db.data.t2wtimes, timeObj);                    
+                                             
+                            var index = _.findIndex(db.data.t2wtimes, function(obj){
+
+                                // search item which fits to the current object which should be inserted
+                                if ((obj.timeKeepingId == timeObj.timeKeepingId) && 
+                                    (obj.startNumber == timeObj.startNumber) && 
+                                    (obj.type == timeObj.type) && 
+                                    (Math.abs(obj.timeMs - timeObj.timeMs) < (config.filterTime*1000) )
+                                   )
+                                {
+                                    //console.log(obj);
+
+                                    switch(timeObj.type)
+                                    {
+                                        case 'FS':
+                                            if (obj.timeMs <= timeObj.timeMs)
+                                                return true;
+                                            break;
+
+                                        case 'BS':
+                                            if (obj.distanceToSplit <= timeObj.distanceToSplit)
+                                                return true;
+                                            break;
+
+                                        case 'LS':
+                                            if (obj.timeMs >= timeObj.timeMs)
+                                                return true;
+                                            break;
+                                    }
+                                }
+
+                                return false;
+                            });
+                            //var index = _.findIndex(db.data.t2wtimes, timeObj);  
     
                             if (index == -1)
                             {
                                 // flag if we send the data to t2w server successfully
                                 timeObj.send2Server = false;
+                                // save insert time
+                                timeObj.insertTime = await getCurrentISOString(config.addSeconds2Time * 1000);
                                 // add to db
                                 db.data.t2wtimes.push(timeObj);
+                            } else {
+                                // save old values
+                                timeObj.send2Server = db.data.t2wtimes[index].send2Server;
+                                timeObj.insertTime  = db.data.t2wtimes[index].insertTime;
+
+                                // check if it is a duplicate value -> if we have already the data in the database do nothing
+                                switch(timeObj.type)
+                                {
+                                    case 'BS':
+                                        // check if equal -> if not equal -> update
+                                        if (timeObj.distance != db.data.t2wtimes[index].distance)
+                                            db.data.t2wtimes[index] = Object.assign({}, timeObj);
+                                        break;
+                                    case 'FS':
+                                    case 'LS':
+                                        // check if equal -> if not equal -> update
+                                        if (timeObj.timeMs != db.data.t2wtimes[index].timeMs)
+                                            db.data.t2wtimes[index] = Object.assign({}, timeObj);
+                                        break;
+                                }
                             }
                         }
                     } else {
@@ -182,7 +239,7 @@ async function filterTimes(times, callback)
         for (var ii=0; ii < times.length; ii++)
         {
             // calculate time in milliseconds from some date
-            times[ii].timeMs = new Date(times[ii].time).valueOf();
+            times[ii].timeMs = new Date(times[ii].time).valueOf();  // #TODO
             times[ii].timeMs = times[ii].timeMs + (config.addSeconds2Time *1000);
             times[ii].time   = new Date(times[ii].timeMs).toISOString();
 
@@ -239,7 +296,6 @@ async function filterTimes(times, callback)
     for (var nn=0; nn<filteredTimesLS.length; nn++){
        
         filteredTimes.push(filteredTimesLS[nn]);
-
         _.last(filteredTimes).type = 'LS';
     }
 
@@ -298,8 +354,18 @@ async function sendTimes2Server()
         var ii=0;
         while (ii < 100) {          
 
-            // check if we have times we should send
-            var sendElem = _.find(db.data.t2wtimes, {'send2Server': false});     
+            // check if we have times we should send -> send2server should be false and also the filter time should have passed 
+            var sendElem = _.find(db.data.t2wtimes, function(obj){
+                // all times are in our times (MEZ + 1h) so add to the utz time 1h
+                var curTimeMs = new Date().valueOf() + (config.addSeconds2Time * 1000);
+                var objTimeMs = new Date(obj.updateTime).valueOf();
+                // check if current time is more than the filter time ahead of the object time to send
+                if ( (obj.send2Server == false) && ( (curTimeMs - objTimeMs) > (config.filterTime * 1000)) )
+                {
+                    //console.log(obj);
+                    return true;                    
+                }
+            });     
 
             // check if we found some new elements
             if (sendElem != undefined)
@@ -339,6 +405,13 @@ async function sendTimes2Server()
 
         await db.write();
     }
+}
+
+
+async function getCurrentISOString(offsetMs)
+{
+    var curDateMs = new Date().valueOf() + offsetMs;
+    return new Date(curDateMs).toISOString();
 }
 
 
@@ -449,5 +522,5 @@ function bbtGetRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
-  }
+}
 
